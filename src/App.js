a7941f2d5a06c54731218611befbd13e79b45f90
mauget <mauget@gmail.com>
2019-08-11 16:19:39 -0400

    Init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import bubbleSort from "./BubbleSort";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const a = [3,5,8,1,398,5,-9,7,321,8,6];
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Bubble Sort</h1>
+                <p>Input array: </p> {a.toString()}
+                <p>Sorted array:</p> {bubbleSort(a).toString()}
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/BubbleSort.js
@@ -0,0 +1,28 @@
+
+const swap = (list, a, b) => {
+    const item = list[b];
+    list[b] = list[a];
+    list[a] = item;
+    return list;
+};
+
+const bubbleSort = (listArg) => {
+    const list = Object.assign([], listArg);
+    let n = list.length;
+    let highIndex = n;
+
+    while ( n > 1 ) {
+        console.log(`n = ${n}`);
+
+        for (let i = 1; i < n; i++){
+            if (list[i-1] > list[i]){
+                swap(list, i-1, i);
+                highIndex = i;
+            }
+        }
+        n = highIndex;
+    }
+    return list;
+};
+
+export default bubbleSort;
